5827e1565bdab3f657fd8589754ecedf544204f1
nitzan-elad <nitzan.elad@gmail.com>
2024-12-04 16:47:40 +0200

    First section of the task Current Progress - Implenebting ZOH values (Q1.d)

--- Task1.py
+++ Task1.py
@@ -7,12 +7,23 @@ import scipy.signal
 def triangle_pulse(t, width=2):
     return np.maximum(0, 1-abs(t)/width/2)
 
+def create_zoh_array(x, t, Ts, start=0):
+    zoh = [x[0]]
+    for i in range(len(t)-1):
+        if start + t[i] % Ts == 0:
+            zoh.append(x[i])
+        else:
+            zoh.append(zoh[i-1])
+    return zoh
 
 
 #Question 1A
 
 wm = 3*math.pi
-t_list = np.arange(0.2, 3, 0.01)
+t_start = 0.2
+t_end = 3
+t_step = 0.01
+t_list = np.arange(t_start, t_end, t_step)
 x = lambda t: 4/(wm*math.pi*t**2)*(math.sin(wm*t))**2*math.cos(wm*t)*math.sin(2*wm*t)
 x_list = [abs(x(t)) for t in t_list]
 
@@ -44,12 +55,10 @@ xF_list = [np.abs(xF(w)) for w in w_list]
 
 w_max = 5*wm
 ws = 2*w_max
-Ts = 2*math.pi/ws
+Ts = 1/15
 
-t_n = np.arange(0.2,3,Ts)
-x_n_list = [x(t) for t in t_n]
-
-plt.plot(t_n, x_n_list, 'o')
+x_ZOH = create_zoh_array(x_list,t_list, Ts, t_start)
+plt.plot(t_list, x_ZOH, 'o')
 plt.xlabel('Time [s]')
 plt.ylabel('|x(t)| [V]')
 plt.title('Absolute Value of x(t)')
