6ccea5cacd09c2c256517591fee7071a67a2505e
nitzan-elad <nitzan.elad@gmail.com>
2024-12-14 17:36:12 +0200

    First section of the task Current Progress - Finished Implementing ZoH of xF(w) - Completed 1D

--- Task1.py
+++ Task1.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import scipy.signal
 
 
-def triangle_pulse(t, width=2):
-    return np.maximum(0, 1-abs(t)/width/2)
+def triangle_pulse(t,center=0, width=2):
+    t_shift = (t - center) / ((width / 2))
+    return np.maximum(0, (1-abs(t_shift)))
 
 def create_sampled(x,t,Ts):
     t_sampled = np.arange(t[0], t[-1], Ts)
@@ -31,63 +32,73 @@ t_list = np.arange(t_start, t_end, t_step)
 x = lambda t: 4/(wm*math.pi*t**2)*(math.sin(wm*t))**2*math.cos(wm*t)*math.sin(2*wm*t)
 x_list = [(x(t)) for t in t_list]
 abs_x_list = [abs(x(t)) for t in t_list]
-# plt.legend()
-# plt.plot(t_list, abs_x_list, label='|x(t)|')
-# plt.xlabel('Time [s]')
-# plt.ylabel('|x(t)| [V]')
-# plt.title('1A - Absolute Value of x(t)')
-# plt.grid(True)
-# plt.show()
+plt.legend()
+plt.plot(t_list, abs_x_list, label='|x(t)|')
+plt.xlabel('Time [s]')
+plt.ylabel('|x(t)| [V]')
+plt.title('1A - Absolute Value of x(t)')
+plt.grid(True)
+plt.show()
 
 
 # Question 1B
+
 w_list = np.arange(-17*math.pi, 17*math.pi, 0.1)
 
-xF = lambda w: (4 * wm / math.pi* 1j) * (-1 * triangle_pulse(((w + 3 * wm) / 2 * wm),4 * wm) - \
-                                     triangle_pulse(((w + 1 * wm) / 2 * wm), 4 * wm) + \
-                                     triangle_pulse(((w - 1 * wm) / 2 * wm), 4 * wm) + \
-                                     triangle_pulse(((w - 1 * wm) / 2 * wm), 4 * wm))
-
-xF_list = [np.abs(xF(w)) for w in w_list]
+xF = lambda w, wm: (1/1j) * (-1 * triangle_pulse(w, -3 * wm, 4 * wm) - \
+                                     triangle_pulse(w, -1 * wm, 4 * wm) + \
+                                     triangle_pulse(w, 1 * wm, 4 * wm) + \
+                                     triangle_pulse(w, 3*wm, 4 * wm))
 
 
-# plt.plot(w_list, xF_list)
-# plt.xlabel('Omega [rad/s]')
-# plt.ylabel('|xF(w)|')
-# plt.title('1B - Absolute Value of xF(w)')
-#plt.grid(True)
-# plt.show()
+
+xF_list = [np.abs(xF(w, wm)) for w in w_list]
+
+
+plt.plot(w_list, xF_list)
+plt.xlabel('Omega [rad/s]')
+plt.ylabel('|xF(w)|')
+plt.title('1B - Absolute Value of xF(w)')
+plt.grid(True)
+plt.show()
 
 #Question 1C
 
 w_max = 5*wm
 ws = 2*w_max
-Ts = 1/15
+Ts = 2*math.pi/ws
 
-# t_ZOH, x_ZOH = create_zoh_array(x_list, t_list, Ts)
-# plt.plot(t_ZOH, x_ZOH, color='orange', label='ZOH of x(t)')
-# plt.plot(t_list, x_list, label='x(t)')
-# plt.legend()
-# plt.xlabel('Time [s]')
-# plt.ylabel('x(t) [V]')
-# plt.title('1C - Zero Order Hold (ZOH)')
-# plt.grid(True)
-# plt.show()
-
-
-xF_ZOH = lambda w,k: xF(w-k*2*np.pi/Ts)
-
-xF_ZOH_list = [0 for w in range(len(w_list))]
-
-for i in range(len(w_list)):
-    for k in [-2,-1]:
-        xF_ZOH_list[i] += xF_ZOH(w_list[i], k)
-    xF_ZOH_list[i] = (1/1j)*np.exp(-1j*w_list[i]*Ts/2)*np.sinc(w_list[i]*Ts/(2*np.pi))*xF_ZOH_list[i]
-
-
-abs_xF_ZOH_list = [abs(xF_ZOH_list[i]) for i in range(len(w_list))]
-plt.plot(w_list, abs_xF_ZOH_list)
+t_ZOH, x_ZOH = create_zoh_array(x_list, t_list, Ts)
+plt.plot(t_ZOH, x_ZOH, color='orange', label='ZOH of x(t)')
+plt.plot(t_list, x_list, label='x(t)')
+plt.legend()
+plt.xlabel('Time [s]')
+plt.ylabel('x(t) [V]')
+plt.title('1C - Zero Order Hold (ZOH)')
+plt.grid(True)
 plt.show()
+
+#Question 1D
+
+xF_ZOH = lambda w: (1/1j)*np.exp(-0.5j*w*Ts)*np.sinc(w/ws)*sum((xF(w-k*ws,wm)) for k in [-2,-1,0,1,2])
+
+xF_ZOH_list = [xF_ZOH(w) for w in w_list]
+
+# for i in range(len(w_list)):
+#     for k in [-2,-1]:
+#         xF_ZOH_list[i] += xF_ZOH(w_list[i], k)
+#     xF_ZOH_list[i] = (1/1j)*np.exp(-1j*w_list[i]*Ts/2)*np.sinc(w_list[i]*Ts/(2*np.pi))*xF_ZOH_list[i]
+
+
+abs_xF_ZOH_list = [abs(xF_ZOH(w)) for w in w_list]
+plt.plot(w_list, abs_xF_ZOH_list)
+plt.xlabel('Omega [rad/s]')
+plt.ylabel('|x_zohF(w)|')
+plt.title('1D - Zero-Order Hold - Absolute Value of xF(w)')
+plt.grid(True)
+plt.show()
+
+
 
 
 
